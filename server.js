981ad7f3cddaae3e711a2c3cce053be986cb7582
A SHIVA KUMAR <shivuas605@gmail.com>
2025-10-30 19:51:29 +0530

    commit 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require(express);
+const express = require('express');
 const app = express();
 
-app.get('/', ()=>{
+app.get('/', (req, res)=>{
     res.json({message:"contact manager api is running"});
 });
 
