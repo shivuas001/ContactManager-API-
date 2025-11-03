44d6fcaef07039ff3f17f4d7e92cf032edeb636a
A SHIVA KUMAR <shivuas605@gmail.com>
2025-11-03 21:32:01 +0530

    commit 6

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const connectDB = async()=>{
      try {
-        const connection = await mongoose.connect('');
+        const connection = await mongoose.connect('mongodb+srv://ContactManagerUser:kUHJsCLWpFNqjHGq@contact-manager-cluster.by9tjl1.mongodb.net/contactmanager-api?appName=Contact-Manager-Cluster');
 
-        console.log('MongoDB connected');
+        console.log(`MongoDB connected ${connection.connection.host}`);
         
         
      } catch (error) {
@@ -11,4 +11,6 @@ const connectDB = async()=>{
         process.exit(1);
     
      }
-}
+};
+
+module.exports = connectDB ;
