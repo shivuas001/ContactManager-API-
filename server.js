4b91b23b6b11124d38df57fe129aaa2e4f8025d0
A SHIVA KUMAR <shivuas605@gmail.com>
2025-11-03 18:56:26 +0530

    commit 4

--- server.js
+++ server.js
@@ -1,35 +1,12 @@
 const express = require('express');
 const app = express();
+const contacts = require('./routes/contactRoutes')
 
 app.get('/', (req, res)=>{
     res.json({message:"contact manager api is running"});
 });
 
-// Display all contacts
-app.get('/contacts', (req, res)=>{
-    res.json({message:"this is for showing all contacts"})
-});
-
-//Display Single contacts
-app.get('/contacts/:id', (req, res)=>{
-    res.json({message:"this is for showing Single contacts"})
-});
-
-//Creating contacts
-app.post('/contacts', (req, res)=>{
-    res.json({message:"this is for Creating contacts"})
-});
-
-//Updating contacts
-app.put('/contacts/:id', (req, res)=>{
-    res.json({message:"this is for Updating contacts"})
-});
-
-//Deleting contacts
-app.delete('/contacts/:id', (req, res)=>{
-    res.json({message:"this is for Deleting contacts"})
-});
-
+app.use('/contacts', contacts);
 
 app.listen(5000,(req, res)=>{
     console.log('server is running in 5000 port');
