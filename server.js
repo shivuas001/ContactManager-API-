c1f59d1648c7bc219c710a52b5b23a422161f95a
A SHIVA KUMAR <shivuas605@gmail.com>
2025-11-03 18:11:07 +0530

    commit 3

--- server.js
+++ server.js
@@ -5,6 +5,32 @@ app.get('/', (req, res)=>{
     res.json({message:"contact manager api is running"});
 });
 
+// Display all contacts
+app.get('/contacts', (req, res)=>{
+    res.json({message:"this is for showing all contacts"})
+});
+
+//Display Single contacts
+app.get('/contacts/:id', (req, res)=>{
+    res.json({message:"this is for showing Single contacts"})
+});
+
+//Creating contacts
+app.post('/contacts', (req, res)=>{
+    res.json({message:"this is for Creating contacts"})
+});
+
+//Updating contacts
+app.put('/contacts/:id', (req, res)=>{
+    res.json({message:"this is for Updating contacts"})
+});
+
+//Deleting contacts
+app.delete('/contacts/:id', (req, res)=>{
+    res.json({message:"this is for Deleting contacts"})
+});
+
+
 app.listen(5000,(req, res)=>{
     console.log('server is running in 5000 port');
 });
