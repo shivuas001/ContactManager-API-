8eb7943cf6c379824453362ebc6457540c335a59
A SHIVA KUMAR <shivuas605@gmail.com>
2025-11-04 19:32:52 +0530

    commit 7

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const Contact = require('../models/contactModel');
+
+
 // Display all contacts
 router.get('/', (req, res)=>{
     res.json({message:"this is for showing all contacts"})
@@ -12,8 +15,17 @@ router.get('/:id', (req, res)=>{
 });
 
 //Creating contacts
-router.post('/', (req, res)=>{
-    res.json({message:"this is for Creating contacts"})
+router.post('/', async(req, res)=>{
+    console.log(req.body);
+
+    const { name, phone, email, address } = req.body ;
+
+    try {
+        const contact = await Contact.create({ name, phone, email, address });
+        res.json(contact);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 //Updating contacts
